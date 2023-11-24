5066a2b26e867ec80f6c27b5f7cd12399fbafab8
Jecheon Ryu <rjc1704@gmail.com>
2023-11-24 12:42:53 +0900

    불필요 파일 제거

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,9 @@
-import letters from "redux/modules/letterSlice";
 import member from "redux/modules/memberSlice";
 import auth from "redux/modules/authSlice";
 
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({ letters, member, auth });
+const rootReducer = combineReducers({ member, auth });
 
 const store = configureStore({
   reducer: rootReducer,
