5fd9f0c8c286432dc84441a7c82e9b61a9bceeb1
Jecheon Ryu <rjc1704@gmail.com>
2023-11-28 11:09:59 +0900

    1~3 단계 완료

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,28 @@
 import Detail from "pages/Detail";
 import Home from "pages/Home";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isLogin ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Navigate replace to="/login" />} />
+          </>
+        )}
       </Routes>
     </BrowserRouter>
   );
